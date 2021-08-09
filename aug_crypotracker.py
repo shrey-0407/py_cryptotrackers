1f682de2114296d68913194582cdbe1f3400930f
SHREYASH MANDAVKAR <72700552+shrey-0407@users.noreply.github.com>
2021-08-09 16:29:40 +0530

    Add files via upload

---
+++ aug_crypotracker.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas_datareader.data as web
+
+from datetime import datetime as dt,timedelta as td
+
+st.title('CRYPTO TRACKER')
+
+opts=st.selectbox('Select Pair',(
+    'BTC-USD'
+    'ETH-USD'
+    'BNB-USD'
+))
+
+prices=web.get_data_yahoo(
+    opts,
+    start=dt.now()-td(days=365),
+    end=dt.now()
+)
+
+st.line_chart(prices)
+
